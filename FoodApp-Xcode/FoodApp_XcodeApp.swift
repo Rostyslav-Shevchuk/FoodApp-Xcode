b1f70e1c2897f8e5a88035d572860aa8d280a259
Rostyslav Shevchuk <rostyslav.shevchuk.123@gmail.com>
2024-02-11 12:46:19 +0200

    Initial Commit

---
+++ FoodApp-Xcode/FoodApp_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp_XcodeApp.swift
+//  FoodApp-Xcode
+//
+//  Created by Ростислав on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
