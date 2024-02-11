691adda2227e35f393b9876043b135bb0429b2f6
Rostyslav <159663154+Rostyslav-Shevchuk@users.noreply.github.com>
2024-02-11 15:21:16 +0200

    add card food

--- FoodApp-Xcode/view/ContentView.swift
+++ FoodApp-Xcode/view/ContentView.swift
@@ -8,12 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var columns = [
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0)
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Menu() {
+                    /*@START_MENU_TOKEN@*/Text("Menu Item 1")/*@END_MENU_TOKEN@*/
+                    /*@START_MENU_TOKEN@*/Text("Menu Item 2")/*@END_MENU_TOKEN@*/
+                    /*@START_MENU_TOKEN@*/Text("Menu Item 3")/*@END_MENU_TOKEN@*/
+                }label: {
+                    Image(systemName: "line.3.horizontal")
+                }
+                
+            }
+            
+            VStack {
+                Text("Work Place")
+                Text("choose your delicious meal")
+            }
+            
+            HStack {
+                Button {
+                    
+                }label: {
+                    Image("")
+                }
+                
+                Button {
+                    
+                }label: {
+                    Image("")
+                }
+                
+                Button {
+                    
+                }label: {
+                    Image("")
+                }
+                
+                Button {
+                    
+                }label: {
+                    Image("")
+                }
+            }
+            
+            Spacer()
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid (columns: columns, spacing: 30) {
+                    ForEach(cardModel.card) { item in
+                        CardView(item: item)
+                    }
+                }
+            }
+            .frame(height: 480)
+            
+            Spacer()
+            
         }
         .padding()
     }
---
+++ FoodApp-Xcode/view/ItemView/CardItemView.swift
@@ -0,0 +1,67 @@
+//
+//  CardItemView.swift
+//  FoodApp-Xcode
+//
+//  Created by Ростислав on 11.02.2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    var item: cardModel
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "circle.circle.fill")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.24705882352941178, green: 0.788235294117647, blue: 0.4745098039215686)/*@END_MENU_TOKEN@*/)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(Color(red: 1.0, green: 0.0, blue: 0.0, opacity: 1.0))
+                }
+                .padding([.leading, .trailing])
+                
+                Spacer()
+                
+                Image(item.Image)
+                
+                Spacer()
+                
+                Text(item.title)
+                    .font(.system(size: 12, weight: .semibold, design: .default))
+                    .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.0))
+                    .padding(.leading, -55)
+                
+                
+                HStack {
+                    Text("$ 10.00")
+                        .font(.system(size: 18, weight: .semibold, design: .default))
+                        .foregroundColor(Color(red: 0.24705882352941178, green: 0.788235294117647, blue: 0.4745098039215686))
+                    
+                    Spacer()
+                    
+                    Image(systemName: "plus.circle.fill")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.24705882352941178, green: 0.788235294117647, blue: 0.4745098039215686)/*@END_MENU_TOKEN@*/)
+                }
+                .padding([.leading, .trailing])
+                
+                Spacer()
+                
+            }
+        }
+        .frame(width: 148, height: 226)
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 15.0, style: .continuous))
+    }
+}
+
+#Preview {
+    CardView(item: cardModel(title: "Vegan Salat", Image: "VeganSalat", price: "$ 10.00"))
+}
